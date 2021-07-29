bfc70711cac22ec11b5f578be25332c6acfaaad2
harry <harry@qq.com>
2021-07-29 10:28:36 +0800

    push HelloWord

---
+++ guigu-common/src/test/java/com/atguigu/test/Test01.java
@@ -0,0 +1,15 @@
+package com.atguigu.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Test01 {
+
+
+    @Test
+    void test01(){
+
+        System.out.println("HelloWord");
+    }
+}
